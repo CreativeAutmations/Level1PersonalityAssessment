b1344b1b98459581e24e00ef6d2306f8e9f7ed30
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2021-06-08 16:46:08 +0530

    Changed table orientation

--- element_engine.js
+++ element_engine.js
@@ -42,12 +42,6 @@ var result_template =
 <table style="font-family: arial, sans-serif; border-collapse: collapse; width: 80%;">
 <tr>
 <td style="color: white; background-color:red; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{red}}</td>
-<td style="color: white; background-color:green; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{green}}</td>
-<td style="color: white; background-color:blue; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{blue}}</td>
-<td style="color: black; background-color:yellow; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{yellow}}</td>
-</tr>
-<tr>
-
 
 <td style="color: white; font-size: medium; background-color:red; border: 1px solid #dddddd;  text-align: center; padding: 8px;">
 
@@ -61,6 +55,12 @@ They automatically find the fastest way to do things and make them fun.
 They change course as often as is needed and aren't likely to let bumps in the road slow them down.
 
 </td>
+</tr>
+
+<tr>
+
+<td style="color: white; background-color:green; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{green}}</td>
+
 <td style="color: white; font-size: medium; background-color:green; border: 1px solid #dddddd;  text-align: center; padding: 8px;">
 Thinker (NT-Green) Personality Style
 Core Value: Competency
@@ -70,14 +70,28 @@ They prefer to make calculated decisions.
 Thinkers explore all aspects of an issue and can't help but suggest new ways of doing things. 
 They need time to think before making decisions. They can be fiercely independent and value their privacy
 </td>
+
+</tr>
+
+<tr>
+
+<td style="color: white; background-color:blue; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{blue}}</td>
+
 <td style="color: white; font-size: medium; background-color:blue; border: 1px solid #dddddd;  text-align: center; padding: 8px;">
 Connector (NF-Blue) Personality Style Core Value: Relationship
 The Connector personality style is considerate, cooperative, and encouraging. 
 Connectors seek harmony and personal connection. 
 They prefer to make decisions that feel good and are in alignment with their values. 
 Connectors naturally interact with others and connect meaning to events. They excel at recognizing strengths in others and place a high importance on person growth. 
-
 </td>
+
+</tr>
+
+<tr>
+
+<td style="color: black; background-color:yellow; border: 1px solid #dddddd;  text-align: center; padding: 8px;">{{yellow}}</td>
+
+
 <td style="color: black; font-size: medium; background-color:yellow; border: 1px solid #dddddd;  text-align: center; padding: 8px;">
 Planner (Si-Gold) Personality Style 
 Core Value: Responsibility
